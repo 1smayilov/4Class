b860d0f1a2be60eceed20be5d977c907cf6f06ed
Elvin Ismayilov <0utsawg@gmail.com>
2024-03-08 23:34:42 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿namespace _4Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //----------------- Continue operatoru ilə 1 - dən 20 - yə kimi olan cüt ədədlərin cəmini tapın. -----------------------------------
+
+
+            //int result = 0;
+            //for (int i = 0; i<20; i++)
+            //{
+            //    if (i % 2 == 1) { 
+            //    continue;
+            //    }
+            //    result += i;
+
+            //}
+            //Console.WriteLine(result);  
+
+
+
+
+
+
+
+            // ---------------------------------------- Daxil etdiyiniz 3 ədəddən ən böyüyünün rəqəmləri cəmini tapın. --------------------------------------
+
+
+
+
+
+            //Console.WriteLine("2 ve ya ondan böyük reqemli 1 eded qeyd");
+            //int num1 = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("2 ve ya ondan böyük reqemli 2-ci eded qeyd");
+            //int num2 = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("2 ve ya ondan böyük reqemli 3-cu eded qeyd");
+            //int num3 = int.Parse(Console.ReadLine());
+
+            //int num = (num1 > num2 && num1 > num3) ? 1 :
+            //       (num2 > num1 && num2 > num3) ? 2 :
+            //       (num3 > num1 && num3 > num2) ? 3 : 0;
+
+
+            //int bigNum = (num == 1) ? num1 :
+            //                (num == 2) ? num2 :
+            //                (num == 3) ? num3 : 0;
+
+
+            //int result = 0;
+            //for (int i = bigNum; i > 0; i /= 10)
+            //{
+
+
+            //    result += i % 10;
+
+
+            //}
+
+            //Console.WriteLine(result);
+
+
+
+
+
+            // ----------------------------------------- Ədədin faktorialının tapılması alqoritmini yazın. ------------------------------------------
+
+
+
+            // Console.WriteLine("Eded qeyd edin ve faktorialini tapin");
+            // int num = Convert.ToInt32(Console.ReadLine());
+
+
+            // int result = 1;
+            //for (int i = 0; i > 0; i++)
+            // {
+            //     result *= i;
+            // }
+
+            //Console.WriteLine(num + "!" +  result);
+            //}
+
+
+
+
+         //   -----------------------------Daxil edilən ədədin sadə və ya mürəkkəb olduğunu bildirən alqoritmi yazın. ------------------------------------
+
+
+
+           //Console.WriteLine("Eded qeyd edin");
+           // int num = Convert.ToInt32(Console.ReadLine());
+
+
+           // string result = (num % 2 == 0 || num % 3 == 0 || num % 3 == 0) ? "Eded murekkebdir" :
+           //                "Eded sadedir";
+
+           // Console.WriteLine(result);
+
+
+            // ----------------------------- Daxil edilən ədədə görə aşağıdakı şəkildə kvadrat düzəldən alqoritm yazın. ---------------------------------------
+
+
+
+            //int hundurluk = 6;
+            //int genislik = 12;
+
+
+            //for (int m = 0; m < genislik; m++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            //Console.WriteLine("");
+
+            //for (int j = 0; j < hundurluk; j++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            //for (int b = 0; b < genislik; b++)
+            //{
+            //    Console.Write("*");
+            //}
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
